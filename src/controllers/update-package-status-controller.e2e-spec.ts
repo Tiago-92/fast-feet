13d61d1fab661cfae8692a13779993395474a30e
Tiago Amaral <tiago.amaral@sc.senai.br>
2025-01-24 16:39:53 -0300

    test: teste E2E para a função de atualizar o status de uma encomenda

---
+++ src/controllers/update-package-status-controller.e2e-spec.ts
@@ -0,0 +1,83 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { PackageStatus } from '@prisma/client'
+import request from 'supertest'
+
+describe('Update Delivred Driver (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let delivererId: string
+  let recipientId: string
+  let packageId: string
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+
+    const deliverer = await prisma.user.create({
+        data: {
+            name: 'Entregador 1',
+            email: 'entregador@fast.com',
+            password: '123456',
+            role: 'DELIVERED_DRIVER',
+        },
+      })
+      delivererId = deliverer.id
+  
+    const recipient = await prisma.user.create({
+        data: {
+            name: 'Destinatário 1',
+            email: 'destinatario@fast.com',
+            password: '123456',
+            role: 'RECIPIENT',
+        },
+    })
+    recipientId = recipient.id
+
+    const packageContent = await prisma.package.create({
+        data: {
+          title: 'Embalagem teste 1',
+          content: 'Embalagem teste 1',
+          status: PackageStatus.DELIVERED,
+          delivererId,
+          recipientId,
+        },
+      })
+      packageId = packageContent.id
+  })
+
+  test('[PATCH] /status/update/:id', async () => {
+    const response = await request(app.getHttpServer())
+      .patch(`/status/update/${packageId}`)
+      .send({
+        status: 'RETURNED',
+      })
+
+    const statusUpdated = await prisma.package.findFirst({
+      where: {
+        status: 'RETURNED',
+      },
+    })
+
+    expect(response.statusCode).toBe(200)
+    expect(statusUpdated).toBeTruthy()
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        _id: expect.objectContaining({
+          value: expect.any(String),
+        }),
+        props: expect.objectContaining({
+          status: 'RETURNED',
+        }),
+      }),
+    )
+  })
+})
